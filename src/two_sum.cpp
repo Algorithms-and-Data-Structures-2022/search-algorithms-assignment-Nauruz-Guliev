d1d3988af64d9537062cbe04f9fbf33eaf725e0a
nauruz <nauruz9248327@gmail.com>
2022-04-21 19:18:58 +0300

    last methods are done

--- src/two_sum.cpp
+++ src/two_sum.cpp
@@ -5,6 +5,14 @@ namespace assignment {
   std::optional<std::pair<int, int>> two_sum(const std::vector<int>& arr, int sum) {
 
     // Tips: для создания пары (pair) используйте функцию std::make_pair
+    int len = arr.size();
+    for (int i  = 0; i<len; i++) {
+      for (int j = i+1; j <len; j++) {
+        if(arr[j] == sum - arr[i]) {
+          return std::make_pair(i,j);
+        }
+      }
+    }
 
     return std::nullopt;
   }
